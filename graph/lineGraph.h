7cf8b49af2bc5a713a7aa0903e8c06a8899e9317
kzkapr1129 <43310385+kzkapr1129@users.noreply.github.com>
2019-04-19 07:06:06 +0900

    実装と一致していないコメントの削除

--- graph/lineGraph.h
+++ graph/lineGraph.h
@@ -8,8 +8,6 @@ public:
 	virtual ~IAdapter() {}
 	virtual int numAxis() const = 0;
 	virtual int length(int axis) = 0;
-
-	// ~1から1の範囲の値を返却する
 	virtual float value(int axis, int index) const = 0;
 };
 
@@ -18,7 +16,6 @@ public:
 	LineGraphDrawer();
 	~LineGraphDrawer();
 
-	// xmin,xmax,ymin,ymaxは-1 ~ 1の範囲で指定
 	void setRange(float ymin, float ymax, int numXAxis = 100);
 	void setAdapter(IAdapter* adapter);
 	const cv::Mat& draw();
